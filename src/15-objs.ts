6aa80036238c220c00bd5bc1b43aadd5e0ed70f6
miguel javela <70237877+migueljav13@users.noreply.github.com>
2022-07-14 12:03:54 -0500

    clase 20 - objetos con tipos

---
+++ src/15-objs.ts
@@ -0,0 +1,21 @@
+(()=>{
+  type Sizes = 'S' | 'M' | 'L' | 'XL';
+  type Product = {
+    title: string,
+    createdAt:Date,
+    stock: number,
+    size?: Sizes
+  }
+  const products: Product[] = [];
+
+  const addProduct = (data:Product) => {
+    products.push(data);
+  }
+
+  addProduct({
+    title:'pro1',
+    createdAt: new Date(),
+    stock: 12
+  });
+  console.log(products)
+})();
